dfe176e1d9fc768a411168001d2fa09167aaefcc
Kimminjae <kimminjae7267@daum.net>
2022-06-16 14:36:13 +0900

    coin_list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import ToDoList from "./MyCoinList";
+import Styles from "./App.module.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((resopnse) => resopnse.json())
+    .then((json) => {
+      setCoins(json)
+      setLoading(false)
+    })
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= {Styles.title}>
+      <ToDoList />
+      <h1>Coin Type : ({coins.length})</h1>
+      <h2>{loading ? <strong>Loading...</strong> : null}</h2>
+
+      <ul>
+        {coins.map((coin) => <li>{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD</li>)}
+      </ul>
+
     </div>
   );
 }
-
 export default App;
---
+++ src/MyCoinList.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+function MyCoinList() {
+  const [toDo, setToDo]  = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray])
+    setToDo("")
+  }
+  return (
+    <div>
+      <h1>My Coins ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange = {onChange}
+          value = {toDo}
+          type = "text"
+          placeholder = "add my coins"
+        />
+        <button>Add </button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
+    </div>
+  );
+}
+export default MyCoinList;
